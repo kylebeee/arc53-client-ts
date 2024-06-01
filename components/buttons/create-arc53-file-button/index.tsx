8060405f0628955d800ffe4413c43d9939fdeb0a
Kyle Breeding <kyle.breeding@algorand.foundation>
2024-06-01 00:11:53 -0700

    added specific styling for create file button

--- components/buttons/create-arc53-file-button/index.tsx
+++ components/buttons/create-arc53-file-button/index.tsx
@@ -3,7 +3,8 @@
 export default function Arc53CreateFileButton() {
     return (
         <button
-
+            className="block p-2 m-2 mt-10 bg-emerald-500 text-white rounded-md"
+            type="button"
         >
             Create File
         </button>
